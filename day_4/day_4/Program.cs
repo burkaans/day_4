effa4f54e92dd6595d5d9ccaf3e9c85c899953b1
burkaans <raimonds.buholts@gmail.com>
2018-05-09 21:00:50 +0300

    2.commit
    4. diena

--- day_4/day_4/Program.cs
+++ day_4/day_4/Program.cs
@@ -10,7 +10,96 @@ namespace day_4
     {
         static void Main(string[] args)
         {
+            int mainigais = 3;
+
+            // <
+            // >
+            // ==
+            // !=
+
+            if(mainigais < 5)
+            {
+                Console.WriteLine("mazaks");
+            }
+            else
+            {
+                Console.WriteLine("lielaks");
+            }
+
+            string mainigais2 = "si ir virkne";
+
+            if (mainigais2 == "si ir virkne")
+            {
+                Console.WriteLine("vienads");
+            }
+            else
+            {
+                Console.WriteLine("nav vienads");
+            }
+
+            mainigais = Convert.ToInt16("45");
+            int a = 5 + mainigais;
+            Console.WriteLine(a);
+
+
+            int mainigais3;
+           string ievade = Console.ReadLine();
+            mainigais3 = Convert.ToInt16(ievade);
+            if(mainigais3 > 5)
+
+            {
+                Console.WriteLine("##");
+            }
+            else
+            {
+                Console.WriteLine("#");
+            }
+
+            int mainigais4;
+            string ievade2 = Console.ReadLine();
+            mainigais4 = Convert.ToInt16(ievade2);
+            if (mainigais4 > 6)
+            {
+                Console.WriteLine("kluda");
+            }
+            else
+            {
+                if(mainigais4 == 6)
+                {
+                    Console.WriteLine("Tas ir sessturis");
+                }
+                else
+                {
+                    if(mainigais4 == 5)
+                    {
+                        Console.WriteLine("piecsturis");
+                    }
+                    else
+                    {
+                        if(mainigais4 == 4)
+                        {
+                            Console.WriteLine("cetrsturis");
+                        }
+                        else
+                        {
+                            if (mainigais4 == 3)
+                            {
+                                Console.WriteLine("trissturis");
+                            }
+                            else
+                            {
+                                if(mainigais4 < 3)
+                                {
+                                    Console.WriteLine("kluda");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             Console.ReadLine();
+                
+                
         }
     }
 }
